aeebde0f37ce5c3e1e9f5bf7546001bfa1988411
Madhu <qq10455006@gmail.com>
2025-09-26 10:29:03 +0800

    fix: Repair Project Safety Warning

--- src/eth/service/contract.ts
+++ src/eth/service/contract.ts
@@ -325,17 +325,18 @@ export default class Contract extends AbstractService {
 
   private localSolcCompile (contractFolderPath: string, filename: string): void {
     const contractPath = path.resolve(contractFolderPath, filename)
-    let output: string
-    const cliCmd = [
-      'solc --base-path . --include-path node_modules/ --optimize',
-      '--evm-version istanbul',
-      `--combined-json abi,bin ${contractPath}`,
-    ].join(' ')
-    try {
-      output = childProcess.execSync(cliCmd, { encoding: 'utf-8' })
-    } catch (error: any) {
-      throw new SolcError(`SolcError❌ An error occurred during compilation: ${error.message}`)
-    }
+    const output = childProcess.execFileSync(
+      'solc',
+      [
+        '--base-path', '.',
+        '--include-path', 'node_modules/',
+        '--optimize',
+        '--evm-version', 'istanbul',
+        '--combined-json', 'abi,bin',
+        contractPath,
+      ],
+      { encoding: 'utf-8' }
+    )
 
     const buildDir = path.resolve(contractFolderPath, 'build')
     if (!fs.existsSync(buildDir)) {
